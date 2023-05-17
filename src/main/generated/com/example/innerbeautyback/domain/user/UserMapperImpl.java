469fdd644c7127b4eacb5da49f1ff26b01217b7f
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-05-17 13:01:13 +0300

    KAAREL - DB creation

--- src/main/generated/com/example/innerbeautyback/domain/user/UserMapperImpl.java
+++ src/main/generated/com/example/innerbeautyback/domain/user/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-17T12:57:27+0300",
+    date = "2023-05-17T13:00:41+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 @Component
