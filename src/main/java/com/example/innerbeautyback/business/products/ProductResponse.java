a92017418395d2a02f40a12b6e1bac17b28b5b08
opilane <student123@gmail.com>
2023-05-19 15:23:28 +0300

    commit

---
+++ src/main/java/com/example/innerbeautyback/business/products/ProductResponse.java
@@ -0,0 +1,33 @@
+package com.example.innerbeautyback.business.products;
+
+import com.example.innerbeautyback.domain.product.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * A DTO for the {@link Product} entity
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse implements Serializable {
+    private Integer productAge;
+    private String productDescription;
+    private LocalDate productAvailableAt;
+    private Integer productPrice;
+    private String productStatus;
+    private String categoryName;
+    private String countryName;
+    private String genderName;
+    private String bloodgroupType;
+    private String imageData;
+
+    public void setId(Integer id) {
+
+
+    }
+}
