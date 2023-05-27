332a6f9eab1aa2d5619a47aa983220451cf7e585
antipaeorg1 <antipaeorg1@hotmail.com>
2023-05-27 12:34:14 +0300

    on endpoint /products/category-all responsebody categoryName added

--- src/main/java/com/example/innerbeautyback/domain/product/ProductMapper.java
+++ src/main/java/com/example/innerbeautyback/domain/product/ProductMapper.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, imports = {Status.class})
 public interface ProductMapper {
 
+    @Mapping(source ="category.name", target = "categoryName")
     @Mapping(source = "id", target = "productId")
     @Mapping(source = "age", target = "productAge")
     @Mapping(source = "description", target = "productDescription")
