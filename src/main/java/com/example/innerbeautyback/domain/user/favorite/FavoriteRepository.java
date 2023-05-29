ad73a86f49d91faa11d8e1e3c6ba5923d46975c8
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-05-29 12:41:26 +0300

    KAAREL - Favorite WIP

--- src/main/java/com/example/innerbeautyback/domain/user/favorite/FavoriteRepository.java
+++ src/main/java/com/example/innerbeautyback/domain/user/favorite/FavoriteRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
 
 
@@ -12,5 +14,10 @@ public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
 
     @Query("select (count(f) > 0) from Favorite f where f.buyer.id = ?1 and f.product.id = ?2")
     boolean favoriteExistsBy(Integer buyerId, Integer productId);
+
+    @Query("select f from Favorite f where f.buyer.id = ?1 and f.product.id = ?2")
+    Optional<Favorite> findFavoriteBy(Integer buyerId, Integer productId);
+
+
 
 }
--- src/main/java/com/example/innerbeautyback/domain/user/favorite/FavoriteService.java
+++ src/main/java/com/example/innerbeautyback/domain/user/favorite/FavoriteService.java
@@ -1,8 +1,9 @@
 package com.example.innerbeautyback.domain.user.favorite;
 
-import com.example.innerbeautyback.domain.user.userproduct.UserProduct;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FavoriteService {
@@ -10,15 +11,8 @@ public class FavoriteService {
     @Resource
     private FavoriteRepository favoriteRepository;
 
-    public void validateFavoriteBy(Integer buyerId, Integer productId) {
-        boolean favoriteExists = favoriteRepository.favoriteExistsBy(buyerId, productId);
-        if (favoriteExists) {
-            throw new RuntimeException("Product already in favorites");
-        }
-    }
-
-    public Favorite getProductBy(Integer productId) {
-        return favoriteRepository.getProductBy(productId);
+    public boolean isProductInFavorites(Integer buyerId, Integer productId) {
+        return favoriteRepository.favoriteExistsBy(buyerId, productId);
     }
 
     public boolean favoriteExistsBy(Integer buyerId, Integer productId) {
@@ -27,5 +21,14 @@ public class FavoriteService {
 
     public void addFavorite(Favorite favorite) {
         favoriteRepository.save(favorite);
+    }
+
+    public Optional<Favorite> findOptionalFavoriteBy(Integer buyerId, Integer productId) {
+        return favoriteRepository.findFavoriteBy(buyerId, productId);
+
+    }
+
+    public void deleteFavorite(Favorite favorite) {
+        favoriteRepository.delete(favorite);
     }
 }
