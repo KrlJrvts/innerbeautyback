519ce5e7461dc254f5b30c8aa40bceaf843fe6a4
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-05-30 09:53:36 +0300

    KAAREL - Favorite Completed!

--- src/main/java/com/example/innerbeautyback/validation/Error.java
+++ src/main/java/com/example/innerbeautyback/validation/Error.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum Error {
     INCORRECT_CREDENTIALS("Wrong email or password", 111),
     USER_UNAVAILABLE("User with this e-mail already exist", 112),
-    PRODUCT_IN_FAVORITES("Product is already in favorites", 211);
+    PRODUCT_IN_FAVORITES("Product is already in favorites", 211),
+    PRODUCT_IN_CART("Product is already in cart", 212);
 
     private final String message;
     private final int errorCode;
