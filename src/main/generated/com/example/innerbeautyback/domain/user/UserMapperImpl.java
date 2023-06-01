826a06d364e232f46c58da858f10e8f73b2ad28f
antipaeorg1 <antipaeorg1@hotmail.com>
2023-06-01 11:50:11 +0300

    ANTI  /products/cart-add fixed

--- src/main/generated/com/example/innerbeautyback/domain/user/UserMapperImpl.java
+++ src/main/generated/com/example/innerbeautyback/domain/user/UserMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-30T09:13:05+0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
+    date = "2023-06-01T11:28:45+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 18.0.2.1 (Oracle Corporation)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
@@ -54,7 +54,6 @@ public class UserMapperImpl implements UserMapper {
         }
 
         user.setPassword( userEditRequest.getUserPassword() );
-        user.setImage( UserMapper.imageDataToImage( userEditRequest.getUserImage() ) );
 
         return user;
     }
